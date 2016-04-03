ba2b2c2cdd65efe276bce4e7873370910b303b74
Iolane Andrade <andrade.icaa@gmail.com>
2016-04-03 13:34:18 -0300

    DrugStoreScreen, GoogleMapDrugstore, GoogleMapHospital: list of variables and functions completed

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugstoreScreen.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugstoreScreen.java
@@ -50,9 +50,6 @@ import static java.security.AccessController.getContext;
 
 public class DrugstoreScreen extends Fragment {
 
-    ArrayList<DrugStore> list;
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState) {
 
@@ -63,15 +60,17 @@ public class DrugstoreScreen extends Fragment {
         final DrugStore drugStore = controller.getDrugstore();
 
         // setting name
-
         TextView nameTextView = (TextView) view.findViewById(R.id.textViewDrugName);
         nameTextView.setText(drugStore.getName());
+
         // Address
         TextView addressTextView = (TextView) view.findViewById(R.id.textViewAddress);
         addressTextView.setText(Html.fromHtml(drugStore.getAddress() + " - " + drugStore.getCity() + " - " + drugStore.getState()));
+
         //CEP
         TextView cepTextView = (TextView) view.findViewById(R.id.textViewCep);
         cepTextView.setText("Cep: " + drugStore.getPostalCode());
+
         // setting telephone
         if (drugStore.getType().equals("FARMACIAPOPULAR")) {
             TextView telephoneTextView = (TextView) view.findViewById(R.id.textViewDrugTel);
@@ -82,9 +81,7 @@ public class DrugstoreScreen extends Fragment {
 
         }
 
-
         //set ratting for drugstore
-
         RatingBar ratingBarFinal = (RatingBar) view.findViewById(R.id.ratingBarFinalDrugstore);
         ratingBarFinal.setRating(drugStore.getRate());
 
@@ -112,7 +109,6 @@ public class DrugstoreScreen extends Fragment {
 
                         }
 
-
                         Looper.loop();
                     }
                 }.start();
@@ -120,11 +116,11 @@ public class DrugstoreScreen extends Fragment {
 
         });
 
+        //Phone call button
             ImageButton phoneCallButton = (ImageButton) view.findViewById(R.id.phonecallButtonDrugstore);
             phoneCallButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) throws SecurityException{
-
 
                     Intent phoneCall = new Intent(Intent.ACTION_CALL);
                     phoneCall.setData(Uri.parse("tel:"+drugStore.getTelephone()));
@@ -132,7 +128,6 @@ public class DrugstoreScreen extends Fragment {
 
                 }
             });
-
 
         return (view);
 
