3d9300e126a21f0b93a7b1812517bb70b0c58916
Iolane Andrade <andrade.icaa@gmail.com>
2016-04-14 19:21:22 -0300

    Stablishment: asserts completed

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/Stablishment.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/Stablishment.java
@@ -1,11 +1,11 @@
 package mds.gpp.saudeemcasa.model;
 
 /**
- * Created by freemanpivo on 9/20/15.
+ * Class name: Stablishment (.java)
+ *
+ * Purpose: This class modeling stablishments like the Hospitals and the Drugstores
  */
-
-
-    public class Stablishment {
+public class Stablishment {
 
     protected String latitude = "";
     protected String longitude = "";
@@ -19,12 +19,15 @@ package mds.gpp.saudeemcasa.model;
     protected String id;
     protected float distance = 0;
 
-
-
     public Stablishment() {
     }
 
     public Stablishment(String name, String telephone) {
+        assert (name != null) : "Receive a null tratment";
+        assert (name.length() > 1) : "Tratment to minor of character in a name ";
+        assert (name != "") : "Tratment empty name";
+        assert (telephone.length() >= 10) : "Tratment to minor of character in telephone";
+
         this.name = name;
         this.telephone = telephone;
     }
@@ -33,6 +36,8 @@ package mds.gpp.saudeemcasa.model;
         return id;
     }
     public void setId(String id) {
+        assert (id != null) : "Receive a null tratment";
+        assert (id != "") : "Receive a empty tratment";
         this.id = id;
     }
 
@@ -41,6 +46,8 @@ package mds.gpp.saudeemcasa.model;
     }
 
     public void setLatitude(String latitude) {
+        assert (latitude != null) : "Receive a null tratment";
+        assert (latitude != "") : "Receive a empty tratment";
         this.latitude = latitude;
     }
 
@@ -49,10 +56,14 @@ package mds.gpp.saudeemcasa.model;
     }
 
     public void setLongitude(String longitude) {
+        assert (longitude != null) : "Receive a null tratment";
+        assert (longitude != "") : "Receive a empty tratment";
         this.longitude = longitude;
     }
 
     public void setCity(String city) {
+        assert (city != null) : "Receive a null tratment";
+        assert (city != "") : "Receive a empty tratment";
         this.city = city;
     }
 
@@ -61,10 +72,13 @@ package mds.gpp.saudeemcasa.model;
     }
 
     public void setState(String state) {
+        assert (state != null) : "Receive a null tratment";
+        assert (state != "") : "Receive a empty tratment";
         this.state = state;
     }
 
     public void setRate(float rate) {
+        assert (rate >= 0) : "Receive a negative tratment";
         this.rate = rate;
     }
 
@@ -89,6 +103,9 @@ package mds.gpp.saudeemcasa.model;
     }
 
     public void setTelephone(String telephone) {
+        assert (telephone != null) : "Receive a null tratment";
+        assert (telephone != "") : "Receive a empty tratment";
+        assert (telephone.length() >= 10) : "Tratment to minor of character in telephone";
         this.telephone = telephone;
     }
 
@@ -97,6 +114,8 @@ package mds.gpp.saudeemcasa.model;
     }
 
     public void setName(String name) {
+        assert (name != null) : "Receive a null tratment";
+        assert (name != "") : "Receive a empty tratment";
         this.name = name;
     }
 
@@ -105,6 +124,8 @@ package mds.gpp.saudeemcasa.model;
     }
 
     public void setType(String type) {
+        assert (type != null) : "Receive a null tratment";
+        assert (type != "") : "Receive a empty tratment";
         this.type = type;
     }
     public float getDistance() {
@@ -112,6 +133,7 @@ package mds.gpp.saudeemcasa.model;
     }
 
     public void setDistance(float distance) {
+        assert (distance >= 0) : "Receive a negative tratment";
         this.distance = distance;
     }
 
