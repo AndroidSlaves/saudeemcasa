040d9cbc1ecb4c36fa4c04bfeff76b983bf81ee5
Pedro Ivo <andradepedroivo@gmail.com>
2016-04-15 01:27:37 -0300

    Adding Dao.java Asserts

--- SaudeEmCasa/app/src/main/java/api/Dao/Dao.java
+++ SaudeEmCasa/app/src/main/java/api/Dao/Dao.java
@@ -16,6 +16,8 @@ public class Dao {
 
     //Método para inserir no banco local
     protected long insertAndClose(SQLiteDatabase sqLiteDatabase, String table, ContentValues values ) {
+        assert (table != null) : "Table name never be null.";
+        assert (table.length() >= 1) : "Table name must have at least one character.";
 
         sqLiteDatabase.insert(table, null, values);
         long resultInsert = 1;
@@ -28,6 +30,9 @@ public class Dao {
     //Metodo para deletar o banco local
     protected long deleteAndClose(SQLiteDatabase sqLiteDatabase, String table) {
         int delete;
+
+        assert (table != null) : "Table name never be null.";
+        assert (table.length() >= 1) : "Table name must have at least one character.";
 
         delete = sqLiteDatabase.delete(table, null, null);
 
