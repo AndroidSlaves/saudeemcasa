8b83facf36aeea422492a2339f297537a80d22e6
Vinicius Carvalho <vinisilvacar@gmail.com>
2016-05-11 22:55:51 -0300

    Adicionando comentários de classe e método - Hospital.java

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/Hospital.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/Hospital.java
@@ -1,11 +1,24 @@
+/*****************************
+ * Class name: Hospital (.java)
+ *
+ * Purpose: Represents a generic hospital entity. Stores the data
+ * that characterize the hospital organization.
+ ****************************/
+
 package mds.gpp.saudeemcasa.model;
 
-
 public class Hospital extends Stablishment {
-    //empty contructor
+    // Empty constructor
     public Hospital() {
     }
 
+    /**
+     * Constructor of hospital
+     *
+     * @param name:String
+     * @param telephone:String
+     *
+     */
     public Hospital(String name, String telephone) {
         super(name, telephone);
 
@@ -19,20 +32,44 @@ public class Hospital extends Stablishment {
     protected String number = "";
     protected String district = "";
 
+    /**
+     * Get the value of attribute number.
+     *
+     * @return number:String
+     *
+     */
     public String getNumber() {
         return number;
     }
 
+    /**
+     * Set the value of attribute number.
+     *
+     * @param number:String
+     *
+     */
     public void setNumber(String number) {
         assert (number != null) : "Null attribute treatment.";
 
         this.number = number;
     }
 
+    /**
+     * Get the string of attribute district.
+     *
+     * @return district:String
+     *
+     */
     public String getDistrict() {
         return district;
     }
 
+    /**
+     * Set the string of attribute district.
+     *
+     * @param district:String
+     *
+     */
     public void setDistrict(String district) {
         assert (district != null) : "Null attribute treatment.";
         assert (district != "") : "Empty district treatment.";
