573febe43a407e551db8670151eaaae9c3e35ee2
Iolane Andrade <andrade.icaa@gmail.com>
2016-05-12 11:45:33 -0300

    DrugStoreDao: comments completed

--- SaudeEmCasa/app/src/main/java/api/Dao/DrugStoreDao.java
+++ SaudeEmCasa/app/src/main/java/api/Dao/DrugStoreDao.java
@@ -1,9 +1,9 @@
-/*****************************
+/************************
  * Class name: DrugStoreDao (.java)
  *
  * Purpose: This class is responsible for making all the operations with the database in regard of
  *          the drugstore information.
- *****************************/
+ ************************/
 
 package api.Dao;
 
@@ -21,17 +21,41 @@ import mds.gpp.saudeemcasa.model.DrugStore;
 
 public class DrugStoreDao extends Dao {
 
+    // Static string that have all the database columns names.
     private static String tableColumns[] = {"latitude", "longitude", "city", "address", "state",
-            "rate", "postalCode", "telephone", "name", "type", "drugstoreGid"};
+                                            "rate", "postalCode", "telephone", "name", "type",
+                                            "drugstoreGid"};
+
+    /*
+     * Instance of DrugstoreDao store whenever it is created so when instantiated again it cannot
+     * create another object.
+     */
     private static DrugStoreDao instance = null;
+
+    // Used to access the table of Drugstore's name.
     private static String tableName = "Drugstore";
 
+    /**
+     * Creates an instace of DrugstoreDao in the context. It's called only by getInstance().
+     *
+     * @param context
+     *              Context type, receive the context where it is in
+     */
     private DrugStoreDao(Context context) {
         assert (context != null) : "context must never be null";
         DrugStoreDao.database = new DatabaseHelper(context);
     }
 
+    /**
+     * Checks if there is a instance of DrugStoreDao, if there isn't, it creates a new one
+     *
+     * @param context
+     *        receive the context to verify the isntance
+     *
+     * @return it return the existing context (Context).
+     */
     public static DrugStoreDao getInstance(Context context) {
+
         if(DrugStoreDao.instance != null) {
 			// Nothing to do.
         } else {
@@ -41,9 +65,18 @@ public class DrugStoreDao extends Dao {
         return DrugStoreDao.instance;
     }
 
+    /**
+     * Verifies if database is empty by selecting one object and verifying if it is null.
+     *
+     * @return cursor, it's a object returned from the database after executing the query command.
+     */
     public boolean isDatabaseEmpty() {
+
+        // Contains the a database command: SELECT.
         String query = "SELECT 1 FROM " + tableName;
         sqliteDatabase = database.getReadableDatabase();
+
+        // Object returned from the database after executing the query command.
         Cursor cursor = sqliteDatabase.rawQuery(query, null);
         boolean isEmpty = false;
 
@@ -52,7 +85,7 @@ public class DrugStoreDao extends Dao {
                 cursor.moveToFirst();
                 isEmpty = true;
             } else {
-                //Nothing to do.
+                /* Nothing to do! */
             }
         } else {
             isEmpty = true;
@@ -61,7 +94,16 @@ public class DrugStoreDao extends Dao {
         return isEmpty;
     }
 
+    /**
+     * Method that takes an drugstore object, convert it into a value table and insert it into
+     * the database
+     *
+     * @param drugStore
+     *
+     * @return outcome of the operation.
+     */
     public boolean insertDrugstore(DrugStore drugStore) {
+
         assert (drugStore != null) : "drugStore object must never be null";
         assert (drugStore.getLatitude() != null) : "latitude field must never be null";
         assert (drugStore.getLongitude() != null) : "longitude field must never be null";
@@ -75,8 +117,10 @@ public class DrugStoreDao extends Dao {
         assert (drugStore.getType() != null) : "type field must never be null";
         assert (drugStore.getId() != null) : "id field must never be null";
 
+        // Open writable Database so element can be inserted.
         SQLiteDatabase sqLiteDatabase = database.getWritableDatabase();
 
+        // Table of values that is used to insert the element into the database.
         ContentValues values = new ContentValues();
 
         values.put(tableColumns[0], drugStore.getLatitude());
@@ -91,17 +135,30 @@ public class DrugStoreDao extends Dao {
         values.put(tableColumns[9], drugStore.getType());
         values.put(tableColumns[10], drugStore.getId());
 
+        // Verify if the table is null
         boolean result = insertAndClose(sqLiteDatabase,tableName, values) > 0;
         return result;
     }
 
+    /**
+     * Creates a list of drugstore objects from data coming from the database.
+     *
+     * @return list of drugstore objects
+     */
     public List<DrugStore> getAllDrugStores() {
+
+        // Contains the a database command: SELECT.
         String query = "SELECT * FROM " + tableName;
+
         List<DrugStore> listDrugstores = new ArrayList<DrugStore>();
+
+        // It indicates which element of the data base it is at.
         Cursor cursor = sqliteDatabase.rawQuery(query, null);
+
         sqliteDatabase = database.getReadableDatabase();
 
         while(cursor.moveToNext()) {
+
             DrugStore drugStore = new DrugStore();
 
             drugStore.setLatitude(cursor.getString(cursor.getColumnIndex(tableColumns[0])));
@@ -122,23 +179,41 @@ public class DrugStoreDao extends Dao {
         return listDrugstores;
     }
 
+    /**
+     * Method that takes a list of drugstore objects and calls the insertDrugstore method for each
+     * one.
+     *
+     * @param drugStoresList
+     *                      receive a list of drugstore
+     *
+     * @return if the all of the drugstores was indexed, it's return true.
+     */
     public boolean insertAllDrugStores(List<DrugStore> drugStoresList) {
+
         assert (drugStoresList != null) : "drugStoresList must never be null";
         assert (drugStoresList.size() > 0) : "drugStoresList field must never be empty";
 
+        // Makes the interaction with each object of the list
         Iterator<DrugStore> index = drugStoresList.iterator();
+
         boolean resultInsertDrugStores = true;
 
         while(index.hasNext()) {
             resultInsertDrugStores = insertDrugstore(index.next());
         }
-
         return resultInsertDrugStores;
     }
 
+    /**
+     * Calls method deleteAndClose from the Dao class, which deletes the database file
+     *
+     * @return the database file deleted.
+     */
     public long deleteAllDrugStores() {
+
         long resultDeleteDrugStores;
         resultDeleteDrugStores = deleteAndClose(sqliteDatabase, tableName);
         return resultDeleteDrugStores;
     }
 }
+
