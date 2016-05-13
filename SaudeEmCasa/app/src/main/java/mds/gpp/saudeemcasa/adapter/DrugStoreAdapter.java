1b24cfc97867acd40007084c1a52cb20dc60c836
Pedro Ivo <andradepedroivo@gmail.com>
2016-05-13 10:43:35 -0300

    Adding Javadoc and some comments

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/adapter/DrugStoreAdapter.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/adapter/DrugStoreAdapter.java
@@ -1,3 +1,9 @@
+/*****************************
+ * Class name: DrugStoreAdapter (.java)
+ *
+ * Purpose: Class used to allow the drugstore object is used by a different contract.
+ ****************************/
+
 package mds.gpp.saudeemcasa.adapter;
 
 import android.content.Context;
@@ -15,13 +21,18 @@ import mds.gpp.saudeemcasa.R;
 import mds.gpp.saudeemcasa.model.DrugStore;
 import mds.gpp.saudeemcasa.model.Hospital;
 
-
 public class DrugStoreAdapter extends ArrayAdapter<DrugStore>   {
     private Context context;
     private ArrayList<DrugStore> lista;
     public static final int COUNT = 15;
 
-    // Setting DrugStoreAdapter constructor
+    /**
+     *
+     * @param context
+     *              Defines the context where Adapter is instantiated.
+     * @param lista
+     *              List of DrugStores to populate adapter view.
+     */
     public DrugStoreAdapter(Context context, ArrayList<DrugStore> lista){
         super(context,0,lista);
 
@@ -33,12 +44,23 @@ public class DrugStoreAdapter extends ArrayAdapter<DrugStore>   {
         this.lista = lista;
     }
 
-    //Set the number of objects to show on list
+    /**
+     *
+     * @return
+     *              Return the quantity of drugstores on list.
+     */
     @Override
     public int getCount() {
         return COUNT;
     }
 
+    /**
+     *
+     * @param position
+     *              Represents the array index.
+     * @return
+     *              Return DrugStore object from array.
+     */
     @Override
     public DrugStore getItem(int position) {
         assert (position >= 0) : "position must never be negative";
@@ -46,6 +68,17 @@ public class DrugStoreAdapter extends ArrayAdapter<DrugStore>   {
         return lista.get(position);
     }
 
+    /**
+     *
+     * @param position
+     *              Arraylist index, populates respective adapter item.
+     * @param convertView
+     *              The old view to reuse.
+     * @param parent
+     *              Represents the style of adapter.
+     * @return
+     *              Return a new view with adapter settings.
+     */
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         assert (position >= 0) : "position must never be null";
@@ -54,6 +87,7 @@ public class DrugStoreAdapter extends ArrayAdapter<DrugStore>   {
 
         return populateAdapter(convertView,position);
     }
+
     /**
      * convert the distance value from meters to kilometers.
      *
@@ -69,6 +103,7 @@ public class DrugStoreAdapter extends ArrayAdapter<DrugStore>   {
         return distance/1000;
 
     }
+
     /**
      * Inflate the list view so that a list can be created with the specified elements.
      *
@@ -78,7 +113,7 @@ public class DrugStoreAdapter extends ArrayAdapter<DrugStore>   {
      * @param position
      *           index in the view list.
      *@return inflated layout.
-     * */
+     */
     public View populateAdapter(View convertView, int position){
         assert (convertView != null) : "convertView must never be null";
         assert (position >= 0) : "position must never be null";
@@ -95,6 +130,7 @@ public class DrugStoreAdapter extends ArrayAdapter<DrugStore>   {
 
         return convertView;
     }
+
     /**
      * set distance value in the item layout
      *
@@ -103,22 +139,21 @@ public class DrugStoreAdapter extends ArrayAdapter<DrugStore>   {
      *
      * @param position
      *           position of the item layout to be accessed.
-     * */
+     */
     public void setDistance(View convertView, int position) {
         assert (position >= 0) : "position must never be null";
         assert (convertView != null) : "convertView must never be null";
+
         if (this.lista.get(position).getDistance() < 1f) {
             // Setting distance of drugstore on list item
             TextView textViewDistance = (TextView) convertView.findViewById(R.id.textView4_item);
             textViewDistance.setText(this.lista.get(position).getDistance() + " m");
-        } else {
+        }else {
             // Setting distance of drugstore on list item
             TextView textViewDistance = (TextView) convertView.findViewById(R.id.textView4_item);
             textViewDistance.setText(convertToKM(this.lista.get(position).getDistance()).toString() + " Km");
         }
 
-        //convertView.setOnClickListener(new OnItemClickListener(position));
-        //return convertView;
     }
 
 }
