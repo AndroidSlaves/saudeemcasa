e7e253775232eedae074d36747d42bcaad717346
Vinicius Carvalho <vinisilvacar@gmail.com>
2016-05-16 16:29:40 -0300

    Adicionando comentários - DrugstoreList.java

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugStoreList.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/DrugStoreList.java
@@ -1,67 +1,62 @@
+/*****************************
+ * Class name: DrugStoreList (.java)
+ *
+ * Purpose: Fill the screen with the list of drugstores, putting the nearest drugstores on the list.
+ ****************************/
+
 package mds.gpp.saudeemcasa.view;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
-import java.util.*;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import api.Exception.ConnectionErrorException;
+import java.util.ArrayList;
+
 import mds.gpp.saudeemcasa.R;
 import mds.gpp.saudeemcasa.adapter.DrugStoreAdapter;
-import mds.gpp.saudeemcasa.adapter.HospitalAdapter;
+import mds.gpp.saudeemcasa.controller.DrugStoreController;
 import mds.gpp.saudeemcasa.helper.GPSTracker;
 import mds.gpp.saudeemcasa.model.DrugStore;
-import mds.gpp.saudeemcasa.model.Hospital;
-import mds.gpp.saudeemcasa.controller.DrugStoreController;
-import mds.gpp.saudeemcasa.model.DrugStore;
-
 
 public class DrugStoreList extends Activity {
-
-    ListView listView;
-    ArrayList<DrugStore> list;
     GPSTracker gps;
-    int drugstore = -1;
+    ArrayList<DrugStore> list;
+    ListView listView;
 
-
+    /**
+     * Create drugstores list from the user's location
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drugstore_list_screen);
 
-        // Initializing list view
-        listView = (ListView) findViewById(R.id.listView);
         gps = new GPSTracker(this);
-
-        // Instancing controller
         final DrugStoreController drugStoreController = DrugStoreController.getInstance(this);
-
-        // Initialize and fill list of drugstore
         list = (ArrayList<DrugStore>) drugStoreController.getAllDrugstores();
+        listView = (ListView) findViewById(R.id.listView);
 
-        if (gps.canGetLocation()) {
+        if(gps.canGetLocation() == true) {
             drugStoreController.setDistance(getApplicationContext(), list);
-            // Initializing new DrugStoreAdapter with list of drugstore
             final DrugStoreAdapter adapter = new DrugStoreAdapter(getApplicationContext(), list);
-
-            // Setting adapter to listView
             listView.setAdapter(adapter);
 
         } else {
-            Toast.makeText(getApplicationContext(), "Voce nao esta conectado ao gps ou a internet!\n Conecte-se para prosseguir.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Voce nao esta conectado ao gps ou a internet!\n" +
+                    " Conecte-se para prosseguir.", Toast.LENGTH_LONG).show();
         }
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView adapterView, View view, int position, long id) {
-        //                list.get(position).setRate((float) 3.3 );//this should be set as the httprequest
                 drugStoreController.setDrugStore(list.get(position));
-                //request from server the rate and set to the drugstore
                 Intent intent = new Intent(getBaseContext(), GoogleMapDrugStore.class);
-
                 startActivity(intent);
             }
         });
