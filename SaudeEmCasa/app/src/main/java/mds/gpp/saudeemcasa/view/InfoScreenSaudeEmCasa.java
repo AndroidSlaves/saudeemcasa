ba8365f18f172dea8df8b126481dea161ef7e6ef
Iolane Andrade <andrade.icaa@gmail.com>
2016-05-20 08:25:28 -0300

    InfoScreenSaudeEmCasa: uncomplicated

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/InfoScreenSaudeEmCasa.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/InfoScreenSaudeEmCasa.java
@@ -23,8 +23,19 @@ public class InfoScreenSaudeEmCasa extends FragmentActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_screen_saude_em_casa);
 
+        final String INFO_TEXT_SAUDE_EM_CASA = "O aplicativo Saude em Casa, visa, facilitar ao " +
+                                               "usuário do Programa Melhor em Casa, formas de " +
+                                               "encontrar Hospitais, Clínicas ou estabelecimentos" +
+                                               " mais próximos ao paciente, atendendo à questao " +
+                                               "de dificuldade de deslocamento dos mesmos. Além" +
+                                               " disso, o aplicativo disponibiliza também as " +
+                                               "localizações de farmácias populares do Brasil, " +
+                                               "para que os usuários possam localizar e ter " +
+                                               "acesso ao possíveis medicamentos de seus " +
+                                               "tratamentos de saude.\n";
+
         TextView nameTextView = (TextView)findViewById(R.id.text_info_saude_em_casa);
-        nameTextView.setText("O aplicativo Saude em Casa, visa, facilitar ao usuário do Programa Melhor em Casa, formas de encontrar Hospitais, Clínicas ou estabelecimentos mais próximos ao paciente, atendendo à questao de dificuldade de deslocamento dos mesmos. Além disso, o aplicativo disponibiliza também as localizações de farmácias populares do Brasil, para que os usuários possam localizar e ter acesso ao possíveis medicamentos de seus tratamentos de saude.\n");
+        nameTextView.setText(INFO_TEXT_SAUDE_EM_CASA);
 
     }
 
