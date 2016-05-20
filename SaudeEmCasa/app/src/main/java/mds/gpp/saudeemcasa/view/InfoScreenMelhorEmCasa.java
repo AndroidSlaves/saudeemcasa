e274b00d2d97163a53d8c93ab084eaf55416f3e9
Iolane Andrade <andrade.icaa@gmail.com>
2016-05-20 08:33:27 -0300

    InfoScreenMelhorEmCasa: uncomplicated

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/InfoScreenMelhorEmCasa.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/InfoScreenMelhorEmCasa.java
@@ -23,14 +23,23 @@ public class InfoScreenMelhorEmCasa extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_screen_melhor_em_casa);
 
-        TextView nameTextView = (TextView) findViewById(R.id.text_info_melhor_em_casa);
-        nameTextView.setText("O programa Melhor em Casa, é um programa relacionado a área de saúde, criado pelo Governo Federal em 2013, com as seguintes propostas:\n" +
-                "\n" +
-                "Atendimento as pessoas com necessidade de reabilitação motora, idosos, pacientes crônicos sem agravamento ou em situação pós-cirúrgica\n" +
-                "\n" +
-                "Melhorar e ampliar a assistência do SUS a pacientes com agravos de saúde que possam receber atendimento em casa, e perto da família,\n" +
-                "\n" +
-                "Contribuir para a recuperação de doenças, visto que, carinho e atenção familiar aliados à adequada assistência em saúde se tornam elementos importantes para a recuperação do paciente.\n");
+        final String INFO_TEXT_MELHOR_EM_CASA = "O programa Melhor em Casa, é um programa " +
+                                                "relacionado a área de saúde, criado pelo " +
+                                                "Governo Federal em 2013, com as seguintes " +
+                                                "propostas:\n \n Atendimento as pessoas com " +
+                                                "necessidade de reabilitação motora, idosos, " +
+                                                "pacientes crônicos sem agravamento ou em " +
+                                                "situação pós-cirúrgica\n \n Melhorar e ampliar" +
+                                                " a assistência do SUS a pacientes com agravos " +
+                                                "de saúde que possam receber atendimento em casa," +
+                                                " e perto da família,\n \n Contribuir para a " +
+                                                "recuperação de doenças, visto que, carinho e " +
+                                                "atenção familiar aliados à adequada assistência" +
+                                                " em saúde se tornam elementos importantes para" +
+                                                " a recuperação do paciente.\n";
 
+
+        TextView nameTextView = (TextView) findViewById(R.id.text_info_melhor_em_casa);
+        nameTextView.setText(INFO_TEXT_MELHOR_EM_CASA);
     }
 }
