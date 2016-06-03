36036f040335575a45ed62275389fc481df4043c
Vinicius Carvalho <vinisilvacar@gmail.com>
2016-06-03 12:12:30 -0300

    Adding complement javadoc comment - Drugstore.java

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/DrugStore.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/model/DrugStore.java
@@ -8,19 +8,21 @@ package mds.gpp.saudeemcasa.model;
 
 public class DrugStore extends Stablishment {
 
-    // Represents the postal code which complements the adress of a drugstore.
+    // Represents the postal code which complements the address of a drugstore.
     private String postalCode = "";
 
     // Method that creates an empty drugstore (constructor)
     public DrugStore() {
-
+        // Empty constructor
     }
 
     /**
      * Method that creates a constructor with the drugstore basic information, name and telephone.
      *
      * @param nameDrugStore
+     *              Pharmaceutical establishment name to be created.
      * @param telephoneDrugStore
+     *              Telephone contact pharmacy which will be created.
      */
     public DrugStore(String nameDrugStore, String telephoneDrugStore) {
         super(nameDrugStore, telephoneDrugStore);
@@ -30,19 +32,24 @@ public class DrugStore extends Stablishment {
         assert(nameDrugStore.length() > 1) : "nameDrugStore must never be one letter";
         assert(telephoneDrugStore != "") : "telephoneDrugStore must never be empty";
         assert(telephoneDrugStore.length() >= 10) : "telephoneDrugStore must never be smaller"
-                                                    +" than 10 characters"; 
-
+                                                    +" than 10 characters";
     }
 
     /**
-     * @return postalCode attribute
+     * Recover storage string postcode.
+     *
+     * @return postalCode
+     *              Code developed by the postal administrations used as guidance location.
      */
     public String getPostalCode() {
         return postalCode;
     }
 
     /**
-     * @param postalCode set attribute.
+     * Assign a postal code with size eight characters.
+     *
+     * @param postalCode
+     *              Set the code used as guidance location.
      */
     public void setPostalCode(String postalCode) {
         assert (postalCode.length() == 8): "postalCode must never be smaller then 0 characters";
