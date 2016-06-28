d221718a543a53286dde0fe0bacf241c18600e80
Vinicius Carvalho <vinisilvacar@gmail.com>
2016-06-28 13:16:05 -0300

    internationalization of informational screens about the programs Saude em Casa, Melhor em casa e Farmacia Popular

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/InfoScreenMelhorEmCasa.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/InfoScreenMelhorEmCasa.java
@@ -23,25 +23,9 @@ public class InfoScreenMelhorEmCasa extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_screen_melhor_em_casa);
 
-        final String INFO_TEXT_MELHOR_EM_CASA = "O programa Melhor em Casa, é um programa " +
-                                                "relacionado a área de saúde, criado pelo " +
-                                                "Governo Federal em 2013, com as seguintes " +
-                                                "propostas:\n \n Atendimento as pessoas com " +
-                                                "necessidade de reabilitação motora, idosos, " +
-                                                "pacientes crônicos sem agravamento ou em " +
-                                                "situação pós-cirúrgica\n \n Melhorar e ampliar" +
-                                                " a assistência do SUS a pacientes com agravos " +
-                                                "de saúde que possam receber atendimento em casa," +
-                                                " e perto da família,\n \n Contribuir para a " +
-                                                "recuperação de doenças, visto que, carinho e " +
-                                                "atenção familiar aliados à adequada assistência" +
-                                                " em saúde se tornam elementos importantes para" +
-                                                " a recuperação do paciente.\n";
-
-
         TextView nameTextView = (TextView) findViewById(R.id.text_info_melhor_em_casa);
         assert (!(nameTextView == null)) : "text view should not bee null";
 
-        nameTextView.setText(INFO_TEXT_MELHOR_EM_CASA);
+        nameTextView.setText(R.string.infoScreenMelhorEmCasa);
     }
 }
