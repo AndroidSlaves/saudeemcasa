48921cb5cfc61b3d88cbfe65aa1ca28b43f507dc
Iolane Andrade <andrade.icaa@gmail.com>
2016-07-01 11:26:02 -0300

    adding loggings in google maps hospital

--- SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/GoogleMapHospital.java
+++ SaudeEmCasa/app/src/main/java/mds/gpp/saudeemcasa/view/GoogleMapHospital.java
@@ -1,5 +1,5 @@
 /*****************************
- * Class name: GoogleMap (.java)
+ * Class name: GoogleMapHospital (.java)
  *
  * Purpose: This class create the view with google maps to show hospital location
  ****************************/
@@ -17,11 +17,15 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import junit.framework.Assert;
 
+import java.util.logging.Logger;
+
 import mds.gpp.saudeemcasa.R;
 import mds.gpp.saudeemcasa.controller.HospitalController;
 
 public class GoogleMapHospital extends FragmentActivity {
 
+    //used to add logging
+    private static final Logger LOG = Logger.getLogger(GoogleMapDrugStore.class.getName());
     // Google map object that contains everything to be show to the user.
     private GoogleMap myGoogleMap = null;
     // Controller that contains the data for proper functioning.
@@ -34,9 +38,12 @@ public class GoogleMapHospital extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         assert (savedInstanceState != null) : "Receive a null treatment";
         super.onCreate(savedInstanceState);
+        LOG.severe("MUST NOT BE NULL");
 
         setContentView(R.layout.activity_google_maps_hospital);
+        LOG.info("THE ACTIVTY WORKS");
         setUpMap();
+        LOG.info("THE GOOGLE MAPS STARTED!!!");
     }
 
     /**
@@ -46,6 +53,7 @@ public class GoogleMapHospital extends FragmentActivity {
     protected void onResume() {
         super.onResume();
         setUpMap();
+        LOG.warning("THE MAPS IS WORKING");
     }
     /**
      * Do a null check to confirm that we have not already instantiated the map. Try to obtain the
@@ -60,7 +68,7 @@ public class GoogleMapHospital extends FragmentActivity {
             myGoogleMap.setTrafficEnabled(true);
             myGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             Assert.assertNotNull(myGoogleMap);
-
+            LOG.severe("MUST NOT BE NULL");
         } else {
             oneLocationMap();
         }
@@ -75,9 +83,17 @@ public class GoogleMapHospital extends FragmentActivity {
         Assert.assertNotNull(hospitalController);
 
         String name = hospitalController.getHospital().getName();
+        LOG.warning("MUST BE NOT EMPTY");
+        LOG.severe("MUST BE NOT NULL");
         String latitude = hospitalController.getHospital().getLatitude();
+        LOG.warning("MUST BE NOT EMPTY");
+        LOG.severe("MUST BE NOT NULL");
         String longitude = hospitalController.getHospital().getLongitude();
+        LOG.warning("MUST BE NOT EMPTY");
+        LOG.severe("MUST BE NOT NULL");
         LatLng hospitalLocation = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+        LOG.warning("MUST BE NOT EMPTY");
+        LOG.severe("MUST BE NOT NULL");
 
         Assert.assertNotNull(name);
         Assert.assertNotNull(latitude);
@@ -85,6 +101,7 @@ public class GoogleMapHospital extends FragmentActivity {
         Assert.assertNotNull(hospitalLocation);
 
         myGoogleMap.addMarker(new MarkerOptions().position(hospitalLocation).title(name));
+        LOG.warning("MUST BE NOT EMPTY");
         myGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hospitalLocation, 10));
 
         Assert.assertNotNull(myGoogleMap);
